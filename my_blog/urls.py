22015c968b5db6ef75c6ec0ff0e4289fe33484a0
Liu Jiashuo <liudada2013@outlook.com>
2016-03-24 00:17:19 +0800

    finish tutorial

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -21,4 +21,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^(?P<idx>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^aboutme/$', views.about_me, name='about_me'),
+    url(r'^tag(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
+    url(r'^search/$', views.blog_search, name='search'),
+    url(r'^feed/$', views.RSSFeed(), name='RSS'),
 ]
